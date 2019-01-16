739ddcb3d44e21ca81c475dda764f274121cb089
Cyprian <Cyp2505@wp.pl>
2019-01-16 12:24:42 +0100

    added setup file to install via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gromologist',
+      version='0.1',
+      description='Library to handle GROMACS topology stuff',
+      url='https://gitlab.com/KomBioMol/gromologist',
+      author='Milosz Wieczor, Cyprian Kleist',
+      author_email='',
+      license='',
+      packages=['gromologist'],
+      zip_safe=False)

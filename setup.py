47392cb7b4e7fb536eb266289bc27efb6d43f1d1
milosz-wieczor <milosz.wieczor@irbbarcelona.org>
2024-12-17 14:13:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gromologist',
-      version='0.310',
+      version='0.320',
       description='Library to handle various GROMACS-related stuff',
       author='Milosz Wieczor',
       author_email='milafternoon@gmail.com',
       license='GNU GPLv3',
-      packages=['gromologist'],
+      packages=find_packages(where="src"),
+      package_dir={"": "src"},
       install_requires=['numpy>=1.10.0'],
       zip_safe=False)

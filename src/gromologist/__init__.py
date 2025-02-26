8d7731003eea42c16c3ec39e2a1a9e61eef5a206
milosz-wieczor <milosz.wieczor@irbbarcelona.org>
2025-02-26 16:31:46 +0100

    Update __init__.py
    updating 0.320 to 0.321

--- src/gromologist/__init__.py
+++ src/gromologist/__init__.py
@@ -2,7 +2,7 @@ from gromologist.Topology import Top
 from gromologist.Section import Section, SectionMol, SectionParam
 from gromologist.Subsection import Subsection, SubsectionAtom, SubsectionBonded, SubsectionHeader, SubsectionParam
 from gromologist.Entries import Entry, EntryAtom, EntryBonded, EntryParam
-from gromologist.Pdb import Pdb, Atom, Traj
+from gromologist.Pdb import Pdb, Residue, Atom, Traj
 from gromologist.DihOpt import DihOpt
 from gromologist.Gmx import *
 from gromologist.Utils import *
